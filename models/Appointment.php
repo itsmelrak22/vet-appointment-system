4bd4b94919fe2cbc2600642153de92eb02af26b2
Karl Angelo Oriel <itsmelrak22@gmail.com>
2023-06-19 23:45:55 +0800

    0619

--- models/Appointment.php
+++ models/Appointment.php
@@ -113,7 +113,7 @@ Class Appointment extends Model {
                                     LEFT JOIN `services` as B
                                     ON A.service_id = B.id
                                     WHERE A.appointment_date LIKE '$formattedDate'
-                                    AND A.status = 'completed'
+                                    AND A.status = 'confirmed'
                                     AND  A.deleted_at IS NULL
                                     ORDER BY A.created_at DESC
                                     ")
